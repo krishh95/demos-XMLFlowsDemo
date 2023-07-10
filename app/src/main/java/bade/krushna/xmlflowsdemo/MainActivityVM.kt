1e7cb926242695290ae4acd978eae167f9f92cc2
Krushna <badekrushna9@gmail.com>
2023-07-10 11:39:19 +0530

    initial commit

---
+++ app/src/main/java/bade/krushna/xmlflowsdemo/MainActivityVM.kt
@@ -0,0 +1,21 @@
+package bade.krushna.xmlflowsdemo
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
+
+class MainActivityVM{ /*: ViewModel() {
+    val item = ""
+    val item1 = flow<String> {
+
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(),null)
+
+    val item2 = MutableStateFlow("")
+
+    suspend fun asyncFun () = viewModelScope.async {
+
+    }.await()*/
+}
